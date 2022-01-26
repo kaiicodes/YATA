53c963cca01ce8b1cb985b3e14ff2586059ce0db
Kai O'Leary <c15354951@mytudublin.ie>
2022-01-26 01:55:30 +0000

    Setup API

--- backend/todos/models.py
+++ backend/todos/models.py
@@ -1,4 +1,12 @@
 from django.db import models
+from django.conf import settings
 
 class Todo(models.Model):
-    pass
+    text = models.CharField(max_length=100)
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_edited = models.DateTimeField(auto_now=True)
+    is_done = models.BooleanField(default=False)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
